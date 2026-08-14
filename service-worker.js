a2a74af886d227a4cb17cf8af1b71a2712c041a2
farid324rahvari-boop <farid324.rahvari@gmail.com>
2026-08-14 23:04:26 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,8 +8,8 @@ const SHELL_FILES = [
   './',
   './index.html',
   './manifest.json',
-  './assets/icon-192.png',
-  './assets/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
